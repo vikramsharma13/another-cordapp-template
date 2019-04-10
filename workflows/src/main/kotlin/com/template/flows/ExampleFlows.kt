b53a99e25734b6505f76d5b63ed3257fb8fa31af
mattbradburyuk <matthew.bradbury@me.com>
2019-04-10 17:07:13 +0100

    flows written and tested

--- workflows/src/main/kotlin/com/template/flows/ExampleFlows.kt
+++ workflows/src/main/kotlin/com/template/flows/ExampleFlows.kt
@@ -3,7 +3,10 @@ package com.template.workflows
 import co.paralleluniverse.fibers.Suspendable
 import com.template.contracts.ExampleContract
 import com.template.states.ExampleState
+import com.template.states.ExampleStateStatus
+import net.corda.core.contracts.Requirements.using
 import net.corda.core.contracts.StateRef
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
 import net.corda.core.node.services.queryBy
@@ -81,9 +84,10 @@ class AmendDraftFlow(val existingStateRef: StateRef,
     @Suspendable
     override fun call() : SignedTransaction{
 
+
         // get existing state details
         val inputStateAndRef = serviceHub.toStateAndRef<ExampleState>(existingStateRef)
-        val inputState = inputStateAndRef.state as ExampleState
+        val inputState = inputStateAndRef.state.data as ExampleState
 
 
         // create output state
@@ -92,11 +96,14 @@ class AmendDraftFlow(val existingStateRef: StateRef,
         // create command
         val command = ExampleContract.Commands.AmendDraft()
 
-        // Build transaction
+        // identify otherParty
+        val me = serviceHub.myInfo.legalIdentities.first()
+        val parties = listOf(inputState.seller, inputState.buyer)
+        val otherParty = parties.filter {it != me}.single()
 
+        // Build transaction
         val txBuilder = TransactionBuilder()
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
-        val me = serviceHub.myInfo.legalIdentities.first()
 
         txBuilder.notary = notary
         txBuilder.addInputState(inputStateAndRef)
@@ -109,13 +116,6 @@ class AmendDraftFlow(val existingStateRef: StateRef,
         // sign
         val stx = serviceHub.signInitialTransaction(txBuilder)
 
-
-        // identify otherParty
-
-        val otherParty = inputState.participants.filter {it != me}
-
-        // todo: fix this Party vs AbstractParty problem
-
         // Finalise
         val session =  initiateFlow(otherParty)
         val ftx = subFlow((FinalityFlow(stx,session)))
@@ -124,7 +124,7 @@ class AmendDraftFlow(val existingStateRef: StateRef,
     }
 }
 
-@InitiatedBy(CreateDraftFlow::class)
+@InitiatedBy(AmendDraftFlow::class)
 class AmendDraftResponderFlow(val otherPartySession: FlowSession) : FlowLogic<SignedTransaction>() {
     @Suspendable
     override fun call(): SignedTransaction {
@@ -134,3 +134,77 @@ class AmendDraftResponderFlow(val otherPartySession: FlowSession) : FlowLogic<Si
 
     }
 }
+
+
+// Agree Flows
+
+@InitiatingFlow
+@StartableByRPC
+class AgreeFlow(val existingStateRef: StateRef) : FlowLogic<SignedTransaction>() {
+    override val progressTracker = ProgressTracker()
+
+    @Suspendable
+    override fun call() : SignedTransaction{
+
+        // get existing state details
+        val inputStateAndRef = serviceHub.toStateAndRef<ExampleState>(existingStateRef)
+        val inputState = inputStateAndRef.state.data as ExampleState
+
+
+        // create output state
+        val outputState = inputState.copy(status = ExampleStateStatus.AGREED)
+
+        // create command
+        val command = ExampleContract.Commands.Agree()
+
+        // identify otherParty
+        val me = serviceHub.myInfo.legalIdentities.first()
+        val parties = listOf(inputState.seller, inputState.buyer)
+        val otherParty = parties.filter {it != me}.single()
+
+        // Build transaction
+        val txBuilder = TransactionBuilder()
+        val notary = serviceHub.networkMapCache.notaryIdentities.first()
+
+        txBuilder.notary = notary
+        txBuilder.addInputState(inputStateAndRef)
+        txBuilder.addOutputState(outputState)
+        txBuilder.addCommand(command, me.owningKey, otherParty.owningKey)
+
+        // verify
+        txBuilder.verify(serviceHub)
+
+        // sign and gather signatures
+        val pstx = serviceHub.signInitialTransaction(txBuilder)
+        val session =  initiateFlow(otherParty)
+        val stx = subFlow(CollectSignaturesFlow(pstx,listOf(session)))
+
+        // Finalise
+        val ftx = subFlow((FinalityFlow(stx,session)))
+
+        return ftx
+    }
+}
+
+@InitiatedBy(AgreeFlow::class)
+class AgreeResponderFlow(val otherPartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+        // Responder flow logic goes here.
+
+        val signTransactionFlow = object: SignTransactionFlow(otherPartySession){
+
+            override fun checkTransaction(stx: SignedTransaction){
+                val output = stx.tx.outputs.single().data
+                requireThat { "output is an ExampleState" using  (output is ExampleState) }
+            }
+        }
+
+        val txId = subFlow(signTransactionFlow).id
+
+        return subFlow(ReceiveFinalityFlow(otherPartySession, expectedTxId = txId))
+
+    }
+}
+
+
